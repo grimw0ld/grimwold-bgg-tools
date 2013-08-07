197463a7dbd0d508da08aae9c4206ff43c0386fa
unknown <ab@ABV.win2k.com>
2013-08-07 16:04:06 +0100

    initial version

---
+++ uniquePlays.php
@@ -0,0 +1,118 @@
+<?php
+
+// --- Get POST variables ---
+
+$mindate=$_POST['mindate'];
+$maxdate=$_POST['maxdate'];
+$trgRnk=$_POST['trgRnk'];
+if(!$trgRnk){$trgRnk=200;}
+if(!$mindate){$mindate="2013-07-01";}
+if(!$maxdate){$maxdate="2013-07-31";}
+
+// --- Define Functions ----
+
+function getBGGrank($gameID)
+  {
+   $gameurl="http://boardgamegeek.com/xmlapi2/thing?id=".$gameID."&stats=1";
+   if ($bggxml =  simplexml_load_file($gameurl))
+     {
+      foreach($bggxml->item as $b)
+        {
+         foreach($b->statistics as $c)
+           {
+            foreach($c->ratings as $d)
+              {
+               foreach($d->ranks as $e)
+                 {
+                  foreach($e->rank as $f)
+                    {
+                     $flag=0;
+                     foreach($f->attributes() as $m => $n)
+                       {
+                        if($n=="boardgame"){$flag=1;}
+                        if($flag==1 && $m=="value"){$flag=0; $BGGrank=$n;}
+                       }
+                    }
+                 }
+              }
+           }
+        }
+     }
+  // $BGGrank=$gameID / 68500 * 200;
+   return $BGGrank;
+  }
+
+
+function listUniques($url,$trgRnk)
+  {
+   $file_handle = fopen($url, "r");
+   $lineNo=0;
+   $file_contents=array();
+   $flg=0;
+   while (!feof($file_handle)) 
+     {
+      $otTxt=trim(strip_tags(fgets($file_handle),'<a>'));
+      $inTxt=strip_tags($otTxt);
+      if($inTxt=='Unique users')
+        {
+         $flg=1;
+        }
+      if(substr($inTxt,-4,4)=='[10]')
+        {
+         $flg=0;
+        }
+      if($flg==1 && $inTxt!='Unique users')
+        {
+         if(!is_numeric($inTxt) && $inTxt != '')
+           {
+            $file_contents[$lineNo]=$otTxt;
+            $lineNo++;
+           }
+         else if(is_numeric($inTxt))
+          {
+            $file_contents[$lineNo]=$inTxt;
+            $lineNo++;
+          }
+        }
+     }
+   fclose($file_handle);
+
+   $lp=0;
+   $len=count($file_contents);
+   while($lp<$len) 
+     {
+      if(!is_numeric($file_contents[$lp]) && $file_contents[$lp] != '')
+       {
+        $workStr=$file_contents[$lp];
+        $urlex=explode("/",$workStr);
+        $gameID=$urlex[2]."&nbsp;";
+        if(getBGGrank($gameID)<$trgRnk){$rnk=0;} else {$rnk=1;}
+        if($rnk==1)
+          {
+           echo "<br />";
+           echo '<a href="http://www.boardgamegeek.com/boardgame/'.$gameID.'">';
+           echo strip_tags($workStr);
+           echo '</a>';
+          } 
+       }
+      else if(is_numeric($file_contents[$lp]) && $rnk==1)
+       {
+        echo "&nbsp;";
+        echo $file_contents[$lp];
+       }
+      $lp++;
+   }
+}
+
+// --- Begin Code ---
+
+$pg=1;
+$tgtpg=2;
+while($pg<$tgtpg)
+  {
+   $url='http://www.boardgamegeek.com/plays/bygame/subtype/boardgame/start/'.$mindate.'/end/'.$maxdate.'/page/'.$pg.'?sortby=distinctusers&subtype=boardgame';
+   listUniques($url,$trgRnk);
+   $pg++;
+  }
+
+?>
---
+++ vgCollectionPlatform.php
@@ -0,0 +1,97 @@
+<?php
+
+// --- Get POST variables ---
+
+$username=$_POST['user'];
+$targetPlat=$_POST['platform'];
+if(!$targetPlat){$targetPlat="Wii";}
+if(!$username){$username="grimwold";}
+
+
+// --- Create Functions for videogame collection ---
+
+function getVGGplatform($VGGuser)
+  {
+   $entry=0;
+   $collection = array();
+   $gameurl="http://videogamegeek.com/xmlapi2/collection?username=".$VGGuser."&subtype=videogame&version=1&own=1";
+   if ($bggxml =  simplexml_load_file($gameurl))
+     {
+      foreach($bggxml->item as $b)
+        {
+         foreach($b->attributes() as $x => $y)
+           {
+            if($x=="objectid")
+              {
+               $gameID=$y;
+               $collection[$entry]['ID'] = $gameID;
+            //    echo "<br />".$collection[$entry]['ID'];
+              }
+           }
+         foreach($b->name as $gameName)
+           {
+            $collection[$entry]['Title'] = $gameName;
+          //   echo " - ".$collection[$entry]['Title'];
+           }
+         foreach($b->version as $c)
+           {
+            foreach($c->item as $d)
+              {
+               foreach($d->thumbnail as $gameThumb)
+                 {
+                  $collection[$entry]['Thumb'] = $gameThumb;
+                 //  echo " - ".$collection[$entry]['Thumb'];
+                 }
+               $lnk=0;
+               foreach($d->link as $e)
+                 {
+                  $flag=0;
+                  foreach($e->attributes() as $m => $n)
+                    {
+                     if($n=="videogameplatform"){$flag=1;}
+                     if($flag==1 && $m=="value")
+                       {
+                        $flag=0;
+                        if($lnk==1)
+                          {
+                           $entry++;
+                           $collection[$entry]['ID'] = $gameID;
+                        //    echo "<br />".$collection[$entry]['ID'];
+                           $collection[$entry]['Title'] = $gameName;
+                        //    echo " - ".$collection[$entry]['Title'];
+                           $collection[$entry]['Thumb'] = $gameThumb;
+                        //    echo " - ".$collection[$entry]['Thumb'];
+                          } 
+                        $lnk=1;
+                        $collection[$entry]['Platform'] = $n;
+                        // echo " - ".$collection[$entry]['Platform'];
+                       }
+                    }
+                 }
+              }
+           }
+         $entry++;
+        }
+     }
+   return $collection;
+  }
+
+$coll=getVGGplatform($username);
+
+$len=count($coll);
+
+echo "<h2>$username</h2>";
+echo "<h2>$targetPlat</h2>";
+$st=0;
+while($st<$len)
+  {
+   if($coll[$st]['Platform']==$targetPlat)
+     {
+      echo "<br />";
+//       echo $coll[$st]['Platform']." - ";
+      echo '<a href="http://www.videogamegeek.com/videogame/'.$coll[$st]["ID"].'">'.$coll[$st]["Title"].'</a>';
+     }
+   $st++;
+  }
+
+?>
